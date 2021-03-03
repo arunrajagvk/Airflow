fdb316419c60c4840ad1bafcbfc26243d21fed40
arunrajagvk <arunrajagvk@gmail.com>
2021-03-03 23:10:58 +0530

    commit

--- Airflow-readme.py
+++ Airflow-readme.py
@@ -3,6 +3,23 @@
 
 Airflow is an orchestrator, not a processing framework, process your gigabytes of data outside of Airflow (i.e. You have a Spark cluster, you use an operator to execute a Spark job, the data is processed in Spark).
 
+// Airflow is an orchestrator allowing you to execute your tasks in the right time, right way at the right order.
+Benefits:
+-- Dynamic - Python 
+-- Highly scalable - using Kubernetes too 
+-- UI - Monitor DP, interact tasks 
+-- Extensible - No need to wait for inbuilt plugin, we can build/ customize our instance 
+
+Core components:
+
+-- Web Server : Flask Server 
+-- Scheduler: Deamon incharge of scheduling DP (Heart of Airflow)
+-- Metastore: DB usually Postgres, responsible to store metadata, tasks and so on 
+-- Executor: How tasks are executed - Using Kubernetes tasks, executes 
+-- Worker: The process/sub-process executes the tasks 
+
+Core Component:
+
 A DAG is a data pipeline, an Operator is a task.
 
 An Executor defines how your tasks are execute whereas a worker is a process executing your task
@@ -16,3 +33,6 @@ If a task fails, check the logs by clicking on the task from the UI and "Logs"
 The Gantt view is super useful to sport bottlenecks and tasks are too long to execute
 
 """
+
+
+
