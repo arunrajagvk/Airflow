66e52e1078b952b7e9c960e82cbac54e7fc389a3
Arunraja <arunrajagvk@users.noreply.github.com>
2021-03-03 22:44:30 +0530

    Airflow- Readme file with details

---
+++ Airflow-readme.py
@@ -0,0 +1,18 @@
+"""
+-- Airflow
+
+Airflow is an orchestrator, not a processing framework, process your gigabytes of data outside of Airflow (i.e. You have a Spark cluster, you use an operator to execute a Spark job, the data is processed in Spark).
+
+A DAG is a data pipeline, an Operator is a task.
+
+An Executor defines how your tasks are execute whereas a worker is a process executing your task
+
+The scheduler schedules your tasks, the web server serves the UI, the database stores the metadata of Airflow.
+
+airflow db init is the first command to execute to initialise Airflow
+
+If a task fails, check the logs by clicking on the task from the UI and "Logs"
+
+The Gantt view is super useful to sport bottlenecks and tasks are too long to execute
+
+"""
